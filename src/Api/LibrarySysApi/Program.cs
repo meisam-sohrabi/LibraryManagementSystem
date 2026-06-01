c2af85366140d7d36140a900a18cbac6b698114b
meisam-sohrabi <meisam.sohrabi2@gmail.com>
2026-06-01 15:35:54 +0330

    cha(production and development mode active for test)

--- src/Api/LibrarySysApi/Program.cs
+++ src/Api/LibrarySysApi/Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddScoped<ValidationFilter<BookAuthorRequestDto>>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
